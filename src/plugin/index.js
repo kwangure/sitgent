8a340f2c17be4b8384c841fd68feeac20a6f893b
kwangure <kaf.wangure@gmail.com>
2022-05-11 03:48:50 +0300

    feat: pass use parameters

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -1,11 +1,11 @@
 import { createTestRunner } from "./test-runner.js";
-
+import { serialize } from "../utils/object.js";
 /**
  * @param {{}} [options]
  * @returns {import("vite").Plugin}
  */
- export default function sveltekitPluginTest(options = {}) {
-    const { playwright } = options;
+export default function sveltekitPluginTest(options = {}) {
+    const { playwright = {} } = options;
     /** @type {import("vite").ResolvedConfig} */
     let config;
     /** @type {import("vite").ViteDevServer} */
@@ -15,13 +15,20 @@ import { createTestRunner } from "./test-runner.js";
         apply: "serve",
         configResolved(_config) {
             config = _config;
+            playwright.use = {
+                ...playwright.use,
+                viteConfig: serialize(config),
+            };
         },
         configureServer(_server) {
             server = _server;
             const protocol = config.server.https ? "https" : "http";
             const port = config.server.port;
-            const baseUrl = `${protocol}://localhost:${port}`;
-            const runner = createTestRunner({ baseUrl, playwright });
+            playwright.use = {
+                ...playwright.use,
+                baseURL: `${protocol}://localhost:${port}`,
+            };
+            const runner = createTestRunner({ playwright });
             server.httpServer?.once("listening", async () => {
                 await runner.run();
             });

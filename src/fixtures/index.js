69cd23a0edf2fe0b82a7dfb11e096858bf4b3ef0
kwangure <kaf.wangure@gmail.com>
2022-05-11 03:47:42 +0300

    feat: add the rest of DTL queries

--- src/fixtures/index.js
+++ src/fixtures/index.js
@@ -1,16 +1,7 @@
 import domTestingLibraryCode from "./dom-testling-library.code.js";
 import { selectors as _selectors } from "playwright";
 
-export async function queries({ selectors }, use) {
-    const queries = {
-        getByRole(role) {
-            return selectors.locator(`role=${role}`);
-        },
-    };
-    await use(queries);
-}
-
-async function selectors({ page }, use) {
+export async function queries({ page }, use) {
     function addScriptTag() {
         return page.addScriptTag({
             content: `\n;console.log('domcontentloaded');\n${domTestingLibraryCode};`,
@@ -19,18 +10,83 @@ async function selectors({ page }, use) {
     }
     await addScriptTag();
     page.on("domcontentloaded", addScriptTag);
-    await _selectors.register("role", () => ({
+    await _selectors.register("alt", () => ({
         query(root, selector) {
-            return __dom_testing_library__.queryByRole(root, selector);
+            return __dom_testing_library__.queryByAltText(root, selector);
         },
         queryAll(root, selector) {
-            return __dom_testing_library__.queryAllByRole(root, selector);
+            return __dom_testing_library__.queryAllByAltText(root, selector);
         },
     }));
-    await use(page);
-};
+
+    await _selectors.register("displayvalue", () => ({
+        query(root, selector) {
+            return __dom_testing_library__.queryByDisplayValue(root, selector);
+        },
+        queryAll(root, selector) {
+            return __dom_testing_library__.queryAllByDisplayValue(root, selector);
+        },
+    }));
+
+    await _selectors.register("label", () => ({
+        query(root, selector) {
+            return __dom_testing_library__.queryByLabelText(root, selector);
+        },
+        queryAll(root, selector) {
+            return __dom_testing_library__.queryAllByLabelText(root, selector);
+        },
+    }));
+
+    await _selectors.register("placeholder", () => ({
+        query(root, selector) {
+            return __dom_testing_library__.queryByPlaceholderText(root, selector);
+        },
+        queryAll(root, selector) {
+            return __dom_testing_library__.queryAllByPlaceholderText(root, selector);
+        },
+    }));
+
+    await _selectors.register("title", () => ({
+        query(root, selector) {
+            return __dom_testing_library__.queryByTitle(root, selector);
+        },
+        queryAll(root, selector) {
+            return __dom_testing_library__.queryAllByTitle(root, selector);
+        },
+    }));
+
+    const queries = {
+        /** Selectors built into playwright */
+        getByRole(role) {
+            return page.locator(`role=${role}`);
+        },
+        getByText(text) {
+            return page.locator(`text=${text}`);
+        },
+
+        /** Using DOM Testing Library */
+        getByAltText(alt) {
+            return page.locator(`alt=${alt}`);
+        },
+        getByDisplayValue(displayValue) {
+            return page.locator(`displayvalue=${displayValue}`);
+        },
+        getByLabelText(label) {
+            return page.locator(`label=${label}`);
+        },
+        getByPlaceholderText(placeholder) {
+            return page.locator(`placeholder=${placeholder}`);
+        },
+        getByTestId(testId) {
+            return page.locator(`testid=${testId}`);
+        },
+        getByTitle(title) {
+            return page.locator(`title=${title}`);
+        },
+    };
+    await use(queries);
+}
 
 export const fixtures = {
     queries,
-    selectors,
 };
